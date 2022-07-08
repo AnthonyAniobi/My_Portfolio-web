2ebb34289be88b33879e4db0628385a16a57b8e4
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-08 16:44:03 +0100

    setup project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/images/android.png';
+import Laptop from './components/laptop/Laptop';
+import Android from './components/android/Android';
+import Server from './components/server/Server';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Server />
+      <Laptop />
+      <Android />
     </div>
   );
 }
