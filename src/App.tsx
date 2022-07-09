8b992105d1495ef3d631c382c82fd95b4c37ee6f
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-09 11:45:57 +0100

    renamed screen to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/images/android.png';
 import Laptop from './components/laptop/Laptop';
 import Android from './components/android/Android';
 import Server from './components/server/Server';
