79b12f1fa336f5cef10b9d1d30393dc1315d8aba
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-11 10:52:43 +0100

    worked on project page

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './projects.scss';
+import { Backend, Web, Mobile } from '../index';
+import { FaReact } from 'react-icons/fa';
+import { SiFlutter, SiPython } from 'react-icons/si';
 
 const Projects = () => {
+
+    const [pageIndex, setPageIndex] = useState(0);
+
+    const currentPage = () => {
+        let selectedPage: JSX.Element;
+
+        switch (pageIndex) {
+            case 1:
+                selectedPage = <Mobile />
+                break;
+            case 2:
+                selectedPage = <Backend />
+                break;
+            default:
+                selectedPage = <Web />
+                break;
+        }
+        return selectedPage
+    }
+
+    const changePage = (index: number) => {
+        setPageIndex(index);
+    }
+
+
     return (
-        <div>Projects</div>
+        <div className='projects'>
+            <div className='projects-sidebar'>
+                <FaReact className={`projects-sidebar-icon ${pageIndex == 0 && 'selected'}`} onClick={() => { changePage(0) }} />
+                <SiFlutter className={`projects-sidebar-icon ${pageIndex == 1 && 'selected'}`} onClick={() => { changePage(1) }} />
+                <SiPython className={`projects-sidebar-icon ${pageIndex == 2 && 'selected'}`} onClick={() => { changePage(2) }} />
+
+            </div>
+
+            <div className="projects-body">
+                {currentPage()}
+            </div>
+        </div>
     )
 }
 
